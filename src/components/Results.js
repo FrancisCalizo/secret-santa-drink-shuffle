129375a375f5be4c1f058a3903cf0f2b5fcb2520
Francis Calizo <francisacalizo@gmail.com>
2019-12-24 11:47:48 -0500

    More Styling

--- src/components/Results.js
+++ src/components/Results.js
@@ -33,15 +33,27 @@ const Results = ({ amount, fools }) => {
     <div className="text-center">
       <Yoda />
       <h1 style={{ color: "#dc3545", fontSize: "3rem" }}>Results</h1>
-      {fools.map((fool, idx) => {
-        return (
-          <div key={idx + 1}>
-            <h2>
-              {fool} - {order[idx]}
-            </h2>
-          </div>
-        );
-      })}
+      <div
+        className="card"
+        style={{
+          width: "80%",
+          marginLeft: "10%",
+          marginRight: "10%",
+          border: "none",
+          boxShadow:
+            " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+        }}
+      >
+        {fools.map((fool, idx) => {
+          return (
+            <div key={idx + 1}>
+              <h2>
+                {fool} - {order[idx]}
+              </h2>
+            </div>
+          );
+        })}
+      </div>
       <div className="grid-2 justify-center">
         <button onClick={handleRedo} className="btn-dark shuffle my-1">
           Reshuffle
