884f6db20e90e1c3f1fecb10df2d189b16f34a39
Francis Calizo <francisacalizo@gmail.com>
2020-01-03 16:18:46 -0500

    Introduce Styled Components to DrunkSanta Component

--- src/components/DrunkSanta.js
+++ src/components/DrunkSanta.js
@@ -1,14 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const DrunkSantaImage = styled.img`
+  width: 17rem;
+  cursor: pointer;
+`;
 
 const DrunkSanta = ({ handleGameChange }) => {
   return (
     <div>
-      <img
-        id="drunk-santa"
+      <DrunkSantaImage
         src={require("../drunk-santa.png")}
         alt="drunk-santa"
         onClick={handleGameChange}
-        style={{ cursor: "pointer" }}
       />
     </div>
   );
