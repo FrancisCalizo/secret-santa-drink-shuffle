420fe2c97a6d7c4e3daf25bf8c5c5db1346a4dbf
Francis Calizo <francisacalizo@gmail.com>
2020-01-07 21:25:16 -0500

    Styled Components to White Elephant

--- src/components/WhiteElephant/WhiteElephant.js
+++ src/components/WhiteElephant/WhiteElephant.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const WhiteElephantImage = styled.img`
+  cursor: pointer;
+  width: 18rem;
+`;
 
 const WhiteElephant = ({ handleGameChange }) => {
   return (
     <div>
-      <img
+      {/* <img
         id="white-elephant"
-        src={require("../../santa-dab.png")}
+        src={require('../../santa-dab.png')}
         alt="white-elephant"
         onClick={handleGameChange}
-        style={{ cursor: "pointer" }}
+        style={{ cursor: 'pointer' }}
+      /> */}
+      <WhiteElephantImage
+        src={require('../../santa-dab.png')}
+        alt="white-elephant"
+        onClick={handleGameChange}
       />
     </div>
   );
